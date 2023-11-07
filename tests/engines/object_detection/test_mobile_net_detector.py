ae0eee60a7b94dfac6ac68e266764d55032194b1
raihua <raihua.dev@gmail.com>
2023-11-08 01:42:32 +1030

    Add test detector instantiable

--- tests/engines/object_detection/test_mobile_net_detector.py
+++ tests/engines/object_detection/test_mobile_net_detector.py
@@ -4,7 +4,9 @@ import os
 # Add the project root to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
-from src.engines.object_detection import mobile_net_detector
+from src.engines.object_detection.mobile_net_detector import MobileNetDetector
 
 import pytest
 
+def test_detector_instantiable():
+    assert MobileNetDetector()
