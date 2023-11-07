9e54e1d1507f114ee9407c06e4f07f23a7db0446
raihua <raihua.dev@gmail.com>
2023-11-08 00:51:37 +1030

    Changed path for test add manager to work

--- tests/managers/image_search_manager/test_manager_add.py
+++ tests/managers/image_search_manager/test_manager_add.py
@@ -4,7 +4,7 @@ import os
 # Add the project root to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
-from src.managers.image_search.image_search_manager import ImageSearchManager
+from managers.image_search_manager import ImageSearchManager
 
 import pytest
 
