2c5bda7af51e01162b8e8cad18b512b197393589
raihua <raihua.dev@gmail.com>
2023-11-07 18:17:57 +1030

    Add method definitions

---
+++ src/managers/image_search/image_search_manager.py
@@ -0,0 +1,20 @@
+class ImageSearchManager():
+    def add(self, image_path: str) -> None:
+        pass
+
+    def search(self, option: str) -> None:
+        pass
+
+    def similar(self, k: int, image_path: str) -> None:
+        pass
+
+    def list(self) -> list:
+        pass
+
+    def set_model(self, model_name: str) -> None:
+        pass
+
+    def set_output_format(self, format: str) -> None:
+        pass
+
+
