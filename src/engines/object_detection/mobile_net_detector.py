505a355f03bd3a2f89ccbc15b3dc120a17763121
raihua <raihua.dev@gmail.com>
2023-11-08 00:51:12 +1030

    Add movile net placeholders

---
+++ src/engines/object_detection/mobile_net_detector.py
@@ -0,0 +1,18 @@
+import base_detector_model
+import object_detector as obj_det
+
+class MobileNetDetector(base_detector_model):
+    def __init__(self, labels):
+        pass
+
+    def add_labels(self, labels):
+        pass
+
+    def encode_labels(self, labels):
+        pass
+
+    def load_model(self):
+        pass
+
+    def detect_objects(self, image):
+        pass
