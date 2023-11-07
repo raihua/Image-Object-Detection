7b55f964b1bf299922cf18586dc952f72f6416ab
raihua <raihua.dev@gmail.com>
2023-11-08 02:26:31 +1030

    Add placeholder tests for image search functions

--- unit_tests/managers/image_search_manager/test_image_search_manager.py
+++ unit_tests/managers/image_search_manager/test_image_search_manager.py
@@ -12,11 +12,35 @@ import pytest
 def manager():
     return ImageSearchManager()
 
-def test_manager_add(manager):
+def test_add(manager):
     manager.add("example_images/image4.jpg")
 
-def test_manager_add_boundary(manager):
+def test_add_boundary(manager):
     with pytest.raises(TypeError):
         manager.add(5)
         manager.add(5.0)
         manager.add()
+
+def test_search(manager):
+    pass
+
+def test_search_boundary(manager):
+    pass
+
+def test_similar(manager):
+    pass
+
+def test_similar_boundary(manager):
+    pass
+
+def test_list(manager):
+    pass
+
+def test_list_boundary(manager):
+    pass
+
+def test_set_model(manager):
+    pass
+
+def test_output_format(manager):
+    pass
