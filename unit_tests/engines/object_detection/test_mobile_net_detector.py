9bfb150ada9b9247c0403c9304eebf103a5337d7
raihua <raihua.dev@gmail.com>
2023-11-08 15:11:53 +1030

    add empty project files

--- unit_tests/engines/object_detection/test_mobile_net_detector.py
+++ unit_tests/engines/object_detection/test_mobile_net_detector.py
@@ -1,5 +1,5 @@
 import pytest
-from .. import project_root
+from ... import project_root
 from src.engines.object_detection.mobile_net_detector import MobileNetDetector
 
 def test_detector_instantiable():
