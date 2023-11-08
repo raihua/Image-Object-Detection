23c499f6f8cc3cecbf0e6194faae3d36971b76a7
raihua <raihua.dev@gmail.com>
2023-11-09 03:11:12 +1030

    implement sqlite add and create table methods

--- src/sqlite_indexing.py
+++ src/sqlite_indexing.py
@@ -1,16 +1,27 @@
-from src.index_strategy import IndexStrategy
 import sqlite3
+
+from src.index_strategy import IndexStrategy
+from src.sqlite_queries import (
+    CREATE__IMAGES_TABLE_QUERY,
+    CREATE_DETECTED_OBJECTS_TABLE_QUERY,
+    INSERT_IMAGE_PATH_PARAM_QUERY
+)
+
 
 class SQLiteIndexing(IndexStrategy):
     def __init__(self, db):
         self.db = db
         self.conn = sqlite3.connect(self.db)
+        self.cursor = self.conn.cursor()
 
-    def add_image_path(self, path):
-        pass
+    def create_table(self):
+        self.cursor.execute(CREATE__IMAGES_TABLE_QUERY)
+        self.cursor.execute(CREATE_DETECTED_OBJECTS_TABLE_QUERY)
+        self.conn.commit()
 
-    def get_detected_objects(self, path):
-        pass
+    def add_image_path(self, image_path):
+        self.cursor.execute(INSERT_IMAGE_PATH_PARAM_QUERY, (image_path,))
+        self.conn.commit()
 
     def get_images_with_all_objects(self, objects):
         pass
