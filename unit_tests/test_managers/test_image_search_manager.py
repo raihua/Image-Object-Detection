931e520d8e71fff8f1b9ed0d59baf1ef5b338694
raihua <raihua.dev@gmail.com>
2023-11-09 00:54:41 +1030

    Add tests for search for manager

--- unit_tests/test_managers/test_image_search_manager.py
+++ unit_tests/test_managers/test_image_search_manager.py
@@ -14,11 +14,27 @@ def test_add_boundary(manager):
         manager.add(5.0)
         manager.add()
 
-def test_search(manager):
-    pass
+@pytest.fixture
+def UC2_terms():
+    return ["car", "person"]
 
-def test_search_boundary(manager):
-    pass
+def test_search_all(manager, UC2_terms):
+    results = manager.search(True, UC2_terms)
+    expected_results = [
+        "example_images/image2.jpg: car,person,truck",
+        "example_images/image5.jpg: car,person,traffic light"
+    ]
+    assert results == expected_results
+
+def test_search_some(manager, UC2_terms):
+    results = manager.search(False, UC2_terms)
+    expected_results = [
+        "example_images/image2.jpg: car,person,truck",
+        "example_images/image3.jpg: chair,person",
+        "example_images/image4.jpg: car",
+        "example_images/image5.jpg: car,person,traffic light"
+    ]
+    assert results == expected_results
 
 def test_similar(manager):
     pass
