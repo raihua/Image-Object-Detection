6ea261ba5bdbc870d6a5da7cb38e9f70b1580dd0
raihua <raihua.dev@gmail.com>
2023-11-09 03:11:21 +1030

    Add sqlite queries file

---
+++ src/sqlite_queries.py
@@ -0,0 +1,19 @@
+CREATE__IMAGES_TABLE_QUERY = """
+CREATE TABLE IF NOT EXISTS Images (
+    image_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image_path TEXT NOT NULL
+);
+"""
+
+CREATE_DETECTED_OBJECTS_TABLE_QUERY = """
+CREATE TABLE IF NOT EXISTS Detected_Objects (
+    detection_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image_id INTEGER,
+    detected_object TEXT NOT NULL,
+    FOREIGN KEY (image_id) REFERENCES Images(image_id)
+);
+"""
+
+INSERT_IMAGE_PATH_PARAM_QUERY = """INSERT INTO Images (image_path) VALUES (?);"""
+
+SELECT_DETECTED_OBJECTS = 
