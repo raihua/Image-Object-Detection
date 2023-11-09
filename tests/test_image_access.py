ee2a715e498be2290c456c78279c2d17a600cf20
raihua <raihua.dev@gmail.com>
2023-11-09 18:40:49 +1030

    change test read image to new param

--- tests/test_image_access.py
+++ tests/test_image_access.py
@@ -3,6 +3,6 @@ from numpy import ndarray
 from src.image_access import ImageAccess
 
 def test_read_image():
-    image_access = ImageAccess("example_images")
-    result = image_access.read_image("image1.jpg")
+    image_access = ImageAccess("example_images/image1.jpg")
+    result = image_access.read_image()
     assert isinstance(result, ndarray)
