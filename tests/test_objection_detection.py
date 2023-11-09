f0aa0a57e2d8dd72aca84c5e0c9fdc6a29c87ea0
raihua <raihua.dev@gmail.com>
2023-11-09 22:04:47 +1030

    add test get model

--- tests/test_objection_detection.py
+++ tests/test_objection_detection.py
@@ -1,13 +1,25 @@
 import pytest
 from src.object_detection import ObjectDetection
+from src.mobile_net_detector import MobileNetDetector
+from src.object_detector import ALL_LABELS
 
 
 @pytest.fixture
 def object_detection():
-    yield ObjectDetection()
+    yield ObjectDetection(MobileNetDetector())
 
 def test_set_model(object_detection):
     object_detection.set_model(None)
     new_model = object_detection.detector_model
     assert new_model is None
+
+def test_get_model(object_detection):
+    model = object_detection.get_model()
+    assert isinstance(model, MobileNetDetector)
+
+# def test_get_labels(object_detection):
+#     labels = object_detection.get_labels()
+#     expected_labels = ALL_LABELS
+#     assert labels == expected_labels
+
 
