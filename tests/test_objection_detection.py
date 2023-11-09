3f0a4a825bd32abe6264f7354d65d69981b10a75
raihua <raihua.dev@gmail.com>
2023-11-09 22:10:34 +1030

    Add all object detection tests

--- tests/test_objection_detection.py
+++ tests/test_objection_detection.py
@@ -1,8 +1,10 @@
 import pytest
 from src.object_detection import ObjectDetection
-from src.mobile_net_detector import MobileNetDetector
+from src.object_detection import MobileNetDetector
 from src.object_detector import ALL_LABELS
+from src.image_access import ImageAccess
 
+all_labels_original = ALL_LABELS
 
 @pytest.fixture
 def object_detection():
@@ -17,9 +19,29 @@ def test_get_model(object_detection):
     model = object_detection.get_model()
     assert isinstance(model, MobileNetDetector)
 
-# def test_get_labels(object_detection):
-#     labels = object_detection.get_labels()
-#     expected_labels = ALL_LABELS
-#     assert labels == expected_labels
+def test_get_labels(object_detection):
+    labels = object_detection.get_labels()
+    expected_labels = all_labels_original
+    assert labels == expected_labels
 
+def test_add_labels(object_detection):
+    labels = ["person"]
+    object_detection.add_labels(labels)
+    result = object_detection.get_labels()
+    assert result[91] == "person"
 
+def test_encode_labels(object_detection):
+    label = ["person"]
+    result = object_detection.encode_labels(label)
+    assert result[0] == True
+
+def test_load_model(object_detection):
+    model = object_detection.load_model()
+    assert callable(model)
+
+def test_detect_objects(object_detection):
+    file_path = "example_images/image4.jpg"
+    image_access = ImageAccess(file_path)
+    image_data = image_access.read_image()
+    result = object_detection.detect_objects(image_data)
+    assert "car" in result
