18fae6532bf23af0afd05c22a7ea600a6ecc0c06
raihua <raihua.dev@gmail.com>
2023-11-09 13:44:48 +1030

    add abstract methods

--- src/mobile_net_detector.py
+++ src/mobile_net_detector.py
@@ -1,19 +1,23 @@
-from src.base_detector_model import BaseDetectorModel
-import src.object_detector as obj_det
+from base_detector_model import BaseDetectorModel
+import object_detector as obj_det
 
 
 class MobileNetDetector(BaseDetectorModel):
     def __init__(self):
-        self.__all_labels = obj_det.ALL_LABELS
         self.__model_path = obj_det.DETECTION_MODEL_DIR
 
     def add_labels(self, labels):
+        last_key = max(obj_det.ALL_LABELS.keys())
         for label in labels:
-            new_key = max(self.__all_labels.keys()) + 1
-            self.__all_labels[new_key] = label
-
-    def get_labels(self):
-        return self.__all_labels
+            last_key += 1
+            obj_det.ALL_LABELS[last_key] = label
+        print(obj_det.ALL_LABELS)
 
     def encode_labels(self, labels):
         obj_det.encode_labels(labels)
+
+    def load_model(self):
+        pass
+    
+    def detect_objects(self, image):
+        pass
