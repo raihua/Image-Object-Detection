57436c1045f0fd4520201247272934e1b73b2e35
raihua <raihua.dev@gmail.com>
2023-11-09 17:09:13 +1030

    add test detect objects

--- tests/test_mobile_net_detector.py
+++ tests/test_mobile_net_detector.py
@@ -1,6 +1,7 @@
 import pytest
 from src.mobile_net_detector import MobileNetDetector
 from src.object_detector import ALL_LABELS
+from src.image_access import ImageAccess
 
 @pytest.fixture
 def mobile_net_detector():
@@ -26,4 +27,11 @@ def test_encode_labels(mobile_net_detector):
 def test_load_model(mobile_net_detector):
     model = mobile_net_detector.load_model()
     assert callable(model)
+
+def test_detect_objects(mobile_net_detector):
+    file_path = "example_images/image4.jpg"
+    image_access = ImageAccess(file_path)
+    image_data = image_access.read_image()
+    result = mobile_net_detector.detect_objects(image_data)
+    assert "car" in result
 
