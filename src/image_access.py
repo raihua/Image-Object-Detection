ae85873b806793b49ad3685a434cf8ffbf6ece3f
raihua <raihua.dev@gmail.com>
2023-11-09 17:09:38 +1030

    refactor image access and add validating

--- src/image_access.py
+++ src/image_access.py
@@ -3,14 +3,34 @@ import os
 
 
 class ImageAccess:
-    def __init__(self, directory):
-        self.__allowed_directory = "example_images"  # Set the allowed directory name
-        if directory != self.__allowed_directory:
-            raise ValueError("Access to directory not permitted")
-        self.__directory = directory
+    def __init__(self, path):
+        # Validate the directory path
+        self.__validate_directory(path)
 
-    def read_image(self, file_name):
-        # Construct the full file path
-        file_path = os.path.join(self.__directory, file_name)
-        image_data = imread(file_path)
+        # Extract the directory and filename components
+        self.__directory, self.__filename = os.path.split(path)
+
+        # Check if the image file exists
+        if not os.path.exists(path):
+            raise FileNotFoundError(f'Image file "{path}" does not exist.')
+
+    def read_image(self):
+        # Construct the full file path using os.path.join()
+        file_path = os.path.join(self.__directory, self.__filename)
+
+        # Read the image data using imread()
+        with open(file_path, 'rb') as f:
+            image_data = imread(f)
+
         return image_data
+
+    def __validate_directory(self, path):
+        # Extract the directory component
+        directory = os.path.split(path)[0]
+
+        # Set the allowed directory name
+        allowed_directory = "example_images"
+
+        # Check if the directory is the allowed directory
+        if directory != allowed_directory:
+            raise ValueError(f"Access to directory '{directory}' not permitted.")
