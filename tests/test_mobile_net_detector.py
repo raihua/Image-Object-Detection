3a110de5f4a54ffe433f4c3327437f810172eeb6
raihua <raihua.dev@gmail.com>
2023-11-09 16:06:25 +1030

    add constructor fixture

--- tests/test_mobile_net_detector.py
+++ tests/test_mobile_net_detector.py
@@ -1,7 +1,12 @@
 import pytest
 from src.mobile_net_detector import MobileNetDetector
 
+@pytest.fixture
+def mobile_net_detector():
+    return MobileNetDetector()
+
 
 def test_add_labels():
-    labels = ["new_label"]
+    labels = ["person"]
+
     
