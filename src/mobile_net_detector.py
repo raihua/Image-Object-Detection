0c0f5764d34945d4935642baa84f4bea621afe8f
raihua <raihua.dev@gmail.com>
2023-11-09 16:06:14 +1030

    added var for obj_det to make it able to be separate

--- src/mobile_net_detector.py
+++ src/mobile_net_detector.py
@@ -3,17 +3,21 @@ import object_detector as obj_det
 
 
 class MobileNetDetector(BaseDetectorModel):
+    def __init__(self):
+        self.__wrappee = obj_det
+    
     def add_labels(self, labels):
-        last_key = max(obj_det.ALL_LABELS.keys())
+        last_key = max(self.__wrappee.ALL_LABELS.keys())
         for label in labels:
             last_key += 1
-            obj_det.ALL_LABELS[last_key] = label
+            self.__wrappee.ALL_LABELS[last_key] = label
 
     def encode_labels(self, labels):
-        obj_det.encode_labels(labels)
+        self.__wrappee.encode_labels(labels)
 
     def load_model(self):
-        obj_det.load_model()
+        self.__wrappee.load_model()
 
     def detect_objects(self, image):
-        obj_det.detect_objects(image)
+        self.__wrappee.detect_objects(image)
+
