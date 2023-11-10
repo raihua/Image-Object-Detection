e03afe4b71ca1ea141459ca7972934449749b276
raihua <raihua.dev@gmail.com>
2023-11-10 23:21:55 +1030

    number to num, shorter name

--- src/number_descending_format.py
+++
@@ -1,9 +0,0 @@
-from format_strategy import FormatStrategy
-from abc import ABC, abstractmethod
-
-class NumberDescendingFormat(FormatStrategy):
-    def __init__(self):
-        super().__init__()
-
-    def format_data(self, result_data):
-        pass
