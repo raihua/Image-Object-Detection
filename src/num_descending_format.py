b7a481eaa2498f9e6feb0a6455bf6e209dffa9b8
raihua <raihua.dev@gmail.com>
2023-11-10 23:28:37 +1030

    add test asc alpha

---
+++ src/num_descending_format.py
@@ -0,0 +1,11 @@
+from format_strategy import FormatStrategy
+from abc import ABC, abstractmethod
+
+
+class NumDescendingFormat(FormatStrategy):
+    def __init__(self):
+        super().__init__()
+
+    def format_data(self, result_data):
+        sorted_dict = dict(sorted(result_data.items(), key=lambda item: item[1], reverse=True))
+        return sorted_dict
