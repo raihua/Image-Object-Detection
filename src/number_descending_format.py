081d28e38a16ddc0923222297013ece78953b55c
raihua <raihua.dev@gmail.com>
2023-11-10 23:13:39 +1030

    add number descending format

---
+++ src/number_descending_format.py
@@ -0,0 +1,9 @@
+from format_strategy import FormatStrategy
+from abc import ABC, abstractmethod
+
+class NumberDescendingFormat(FormatStrategy):
+    def __init__(self):
+        super().__init__()
+
+    def format_data(self, result_data):
+        pass
