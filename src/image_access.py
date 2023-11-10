400149ad8c8cdb4ecdc8dc21a717902fd84a61bf
raihua <raihua.dev@gmail.com>
2023-11-11 00:04:54 +1030

    removed redundant comments

--- src/image_access.py
+++ src/image_access.py
@@ -4,12 +4,10 @@ import os
 
 class ImageAccess:
     def __init__(self, path):
-        # Validate the directory path
         self.__validate_directory(path)
 
         self.__path = path
 
-        # Check if the image file exists
         if not os.path.exists(path):
             raise FileNotFoundError(f'Image file "{path}" does not exist.')
 
@@ -27,9 +25,7 @@ class ImageAccess:
         # Extract the directory component
         directory = os.path.split(path)[0]
 
-        # Set the allowed directory name
         allowed_directory = "example_images"
 
-        # Check if the directory is the allowed directory
         if directory != allowed_directory:
             raise ValueError(f"Access to directory '{directory}' not permitted.")
