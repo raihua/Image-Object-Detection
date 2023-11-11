2005ca8eb80fbc0b6d5b5ed96049a9f953a4ef95
raihua <raihua.dev@gmail.com>
2023-11-11 14:43:35 +1030

    add sorting to list within tuple

--- src/alphabetical_ascending_format.py
+++ src/alphabetical_ascending_format.py
@@ -7,6 +7,8 @@ class AlphabeticalAscendingFormat(FormatStrategy):
 
     def format_data(self, result_data) -> tuple:
         sorted_result_data = tuple(
-            sorted(result_data, key=lambda item: item[0], reverse=False)
+            # Sorts tuples in alphabetical order based on item[0] (path),
+            # while also sorting the detected object list alphabetically.
+            (item[0], sorted(item[1])) for item in sorted(result_data, key=lambda item: item[0])
         )
         return sorted_result_data
