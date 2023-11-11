ea00d5bc0c18e303d1be1295b8c47a018852a7f3
raihua <raihua.dev@gmail.com>
2023-11-12 02:21:57 +1030

    add test invalid directory

--- tests/test_image_access.py
+++ tests/test_image_access.py
@@ -12,3 +12,7 @@ def test_read_image(image_access):
     result = image_access.read_image("example_images/image1.jpg")
     assert isinstance(result, np.ndarray)
 
+
+def test_validate_directory(image_access):
+    with pytest.raises((ValueError, FileNotFoundError)):
+        image_access.read_image("imcryingoutloud/images1.jpg")
