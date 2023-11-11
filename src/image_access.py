509cf32b7b7847856a867099c8c1b9bbcc420005
raihua <raihua.dev@gmail.com>
2023-11-12 02:21:51 +1030

    removed unused function

--- src/image_access.py
+++ src/image_access.py
@@ -9,11 +9,6 @@ class ImageAccess:
         image_data = mpimg.imread(path)
         return image_data
 
-    # TODO assess whether this is needed
-    def flatten_and_reshape_image(self, image_data) -> np.ndarray:
-        flattened_image = image_data.flatten().reshape(1, -1)
-        return flattened_image
-
     def __validate_directory(self, path):
         if not os.path.exists(path):
             raise FileNotFoundError(f'Image file "{path}" does not exist.')
