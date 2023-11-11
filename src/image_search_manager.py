607a70ac73c7518df94ed1e606ba64a952d326ac
raihua <raihua.dev@gmail.com>
2023-11-11 14:43:48 +1030

    add method complete

--- src/image_search_manager.py
+++ src/image_search_manager.py
@@ -9,8 +9,10 @@ from alphabetical_ascending_format import AlphabeticalAscendingFormat
 from sqlite_indexing import SQLiteIndexing
 from mobile_net_detector import MobileNetDetector
 
+import sqlite3
 
-class ImageSearchManager:
+
+class ImageSearchManager():
     def __init__(self):
         self.__output_formatter = OutputFormatter()
         self.__object_detection = ObjectDetection()
@@ -18,8 +20,22 @@ class ImageSearchManager:
         self.__image_access = ImageAccess()
         self.__matching_engine = MatchingEngine()
 
+        self.__index_access.set_strategy(SQLiteIndexing(sqlite3.connect(":memory:")))
+        self.__object_detection.set_model(MobileNetDetector())
+
     def add(self, image_path) -> str:
         self.__index_access.add_image_path(image_path)
         image_data = self.__image_access.read_image(image_path)
-        detected_objects = self.__object_detection.detect_objects(image_data)
+        detected_objects = sorted(self.__object_detection.detect_objects(image_data))
         self.__index_access.add_detected_objects(image_path, detected_objects)
+        result_str = "Detected objects " + ",".join(detected_objects)
+        return result_str
+    
+    def search(self, k, terms) -> str:
+        result_str = None
+        if k:
+            result_str = self.__index_access.get_images_with_all_objects(terms)
+        else:
+            result_str = self.__index_access.get_images_with_some_objects(terms)
+
+        
