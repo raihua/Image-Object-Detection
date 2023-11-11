f7e6ff20354499735792f7d56eb54b0e830cca3f
raihua <raihua.dev@gmail.com>
2023-11-11 14:47:17 +1030

    set strategy out. test for float data with flaot fixture

--- tests/test_output_formatter.py
+++ tests/test_output_formatter.py
@@ -15,20 +15,33 @@ def num_desc_format():
 
 
 @pytest.fixture
-def output_formatter(alpha_asc_format):
-    strategy = alpha_asc_format
-    yield OutputFormatter(strategy)
+def output_formatter(num_desc_format):
+    strategy = num_desc_format
+    output_formatter = OutputFormatter()
+    output_formatter.set_strategy(strategy)
+    yield output_formatter
 
 
 @pytest.fixture
-def image_data():
-    image_data = (
+def float_image_data():
+    float_image_data = (
         ("example_images/image3.jpg", 1.0000),
         ("example_images/image6.jpg", 0.5000),
         ("example_images/image1.jpg", 0.4082),
     )
 
-    return image_data
+    return float_image_data
+
+
+@pytest.fixture
+def str_image_data():
+    str_image_data = (
+        ("example_images/image3.jpg", ["person", "chair"]),
+        ("example_images/image2.jpg", ["truck", "person", "car"]),
+        ("example_images/image1.jpg", ["chair", "dining table", "potted plant"]),
+    )
+
+    return str_image_data
 
 
 def test_set_strategy(output_formatter, num_desc_format):
@@ -36,33 +49,34 @@ def test_set_strategy(output_formatter, num_desc_format):
     assert isinstance(output_formatter._OutputFormatter__strategy, NumDescendingFormat)
 
 
-def test_format_data(output_formatter, image_data):
-    result = output_formatter.format_data(image_data)
+def test_format_data(output_formatter, str_image_data, alpha_asc_format):
+    output_formatter.set_strategy(alpha_asc_format)
+    result = output_formatter.format_data(str_image_data)
 
     expected_result = (
-        ("example_images/image1.jpg", 0.4082),
-        ("example_images/image3.jpg", 1.0000),
-        ("example_images/image6.jpg", 0.5000),
+        ("example_images/image1.jpg", ["chair", "dining table", "potted plant"]),
+        ("example_images/image2.jpg", ["car", "person", "truck"]),
+        ("example_images/image3.jpg", ["chair", "person"]),
     )
 
     assert result == expected_result
 
 
-def test_validate_k_no_exception(output_formatter, image_data):
+def test_validate_k_no_exception(output_formatter, float_image_data):
     k = 3
-    result = output_formatter._OutputFormatter__validate_k(k, image_data)
+    result = output_formatter._OutputFormatter__validate_k(k, float_image_data)
     assert result == k
 
 
-def test_validate_k_less_than_2_exception(output_formatter, image_data):
+def test_validate_k_less_than_2_exception(output_formatter, float_image_data):
     k = 1
     with pytest.raises(
         ValueError, match="k must be greater than or equal to 2 if provided"
     ):
-        output_formatter._OutputFormatter__validate_k(k, image_data)
+        output_formatter._OutputFormatter__validate_k(k, float_image_data)
 
 
-def test_get_top_k_results_alpha_acs(output_formatter, image_data):
+def test_get_top_k_results_alpha_acs(output_formatter, float_image_data):
     formatted_data = (
         ("example_images/image1.jpg", 0.4082),
         ("example_images/image2.jpg", 1.0000),
