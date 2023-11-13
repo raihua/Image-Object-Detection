e2baf70b8b19e61c532ba12e938799db2a32314c
raihua <raihua.dev@gmail.com>
2023-11-13 18:42:16 +1030

    refactored cosine

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,12 +1,9 @@
 import sklearn.metrics.pairwise as smp
 from similarity_strategy import SimilarityStrategy
 
-
 class CosineSimilarity(SimilarityStrategy):
     def __init__(self):
         super().__init__()
 
     def calculate_similarity(self, image, image_compared) -> float:
-        similarity = smp.cosine_similarity(image, image_compared)
-        float_similarity = similarity[0, 0]
-        return float_similarity
+        return smp.cosine_similarity(image, image_compared)[0, 0]
