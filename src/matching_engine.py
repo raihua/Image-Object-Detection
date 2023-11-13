a1020bf06740c2abd2acb71b4a30a1006378e9bb
raihua <raihua.dev@gmail.com>
2023-11-13 13:00:26 +1030

    Refactor matching engine

--- src/matching_engine.py
+++ src/matching_engine.py
@@ -10,19 +10,18 @@ class MatchingEngine:
 
     def __calculate_similarity(self, flat_ndarray1, flat_ndarray2) -> float:
         return self.__strategy.calculate_similarity(flat_ndarray1, flat_ndarray2)
+    
+    def _reshape_image(self, image):
+        image_array = np.array(image)
+        return image_array.reshape(1, -1)
 
     def execute_matching(self, image1, encoded_objects_with_paths) -> dict:
         results = {}
-        image1_array = np.array(image1)
-        image1_array_reshaped = image1_array.reshape(1, -1)
+        image1_reshaped = self._reshape_image(image1)
+
         for img_path, detected_objs in encoded_objects_with_paths.items():
-            # Convert detected_objs to numpy arrays
-            detected_objs_array = np.array(detected_objs)
-
-            detected_objs_reshaped = detected_objs_array.reshape(1, -1)
-
-            similarity_score = self.__calculate_similarity(
-                image1_array_reshaped, detected_objs_reshaped
-            )
+            detected_objs_reshaped = self._reshape_image(detected_objs)
+            similarity_score = self.__calculate_similarity(image1_reshaped, detected_objs_reshaped)
             results[img_path] = float(similarity_score)
+
         return results
