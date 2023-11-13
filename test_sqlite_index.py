60f05edfe02e13f7383e6b90bcecfc34767c7858
raihua <raihua.dev@gmail.com>
2023-11-13 22:37:12 +1030

    fixed test with new sqlite3

--- test_sqlite_index.py
+++ test_sqlite_index.py
@@ -1,61 +1,117 @@
 import pytest
-from unittest.mock import Mock, create_autospec, patch, call
 import sqlite3
 from sqlite_indexing import SQLiteIndexing
-from sqlite_queries import (
-    CREATE__IMAGES_TABLE_QUERY,
-    CREATE_DETECTED_OBJECTS_TABLE_QUERY,
-    INSERT_IMAGE_PATH_PARAM_QUERY,
-    SELECT_INCLUDE_ALL_DETECTED,
-    SELECT_INCLUDE_SOME_DETECTED,
-    INSERT_DETECTED_OBJECTS_PARAM_QUERY,
-    SELECT_ALL_IMAGES_OBJECTS_QUERY,
-    SELECT_COUNT_PATH,
-    SELECT_COUNT_DETECTED,
-)
-# Mocking the sqlite3 connection
+
+
 @pytest.fixture
-def mock_sqlite3(monkeypatch):
-    mock_conn = create_autospec(sqlite3.Connection, instance=True)
-    mock_cursor = create_autospec(sqlite3.Cursor, instance=True)
-    mock_conn.cursor.return_value = mock_cursor
-    monkeypatch.setattr('sqlite3.connect', Mock(return_value=mock_conn))
-    return mock_conn, mock_cursor
+def sqlite_indexing_connection_cursor():
+    # Creating an in-memory SQLite database and executing table creation statements.
+    sqlite_indexing = SQLiteIndexing()
+    sqlite_indexing._SQLiteIndexing__conn = sqlite3.connect(":memory:")
+    sqlite_indexing._SQLiteIndexing__create_table()
+    conn = sqlite_indexing._SQLiteIndexing__conn
+    cursor = sqlite_indexing._SQLiteIndexing__conn.cursor()
+    yield sqlite_indexing, conn, cursor
 
-# Tests for SQLiteIndexing
-def test_create_table(mock_sqlite3):
-    mock_conn, mock_cursor = mock_sqlite3
-    indexing = SQLiteIndexing()
-    indexing._SQLiteIndexing__conn = mock_conn
 
-    mock_cursor.execute.assert_has_calls([
-        call(CREATE__IMAGES_TABLE_QUERY),
-        call(CREATE_DETECTED_OBJECTS_TABLE_QUERY)
-    ])
-    mock_conn.commit.assert_called_once()
+@pytest.fixture
+def insert_initial_data(sqlite_indexing_connection_cursor):
+    _, conn, cursor = sqlite_indexing_connection_cursor
+    # Insert image paths and associated objects in 'Detected_Objects' table
+    image_path = "example_images/image1.jpg"
+    cursor.execute(
+        "INSERT INTO Images (image_path) VALUES (?);",
+        (image_path,),  # Ensure the value is a tuple
+    )
+    cursor.execute(
+        "INSERT INTO Detected_Objects (image_path, detected_object) VALUES (?, ?);",
+        (image_path, "chair"),
+    )
+    cursor.execute(
+        "INSERT INTO Detected_Objects (image_path, detected_object) VALUES (?, ?);",
+        (image_path, "dining table"),
+    )
+    conn.commit()
 
-def test_add_image_path(mock_sqlite3):
-    mock_conn, mock_cursor = mock_sqlite3
-    mock_cursor.fetchone.return_value = [0]  # Simulating path not existing
 
-    indexing = SQLiteIndexing()
-    indexing.add_image_path('test.jpg')
+def test_tables_exist(sqlite_indexing_connection_cursor):
+    _, _, cursor = sqlite_indexing_connection_cursor
 
-    mock_cursor.execute.assert_called_with(INSERT_IMAGE_PATH_PARAM_QUERY, ('test.jpg',))
-    mock_conn.commit.assert_called_once()
+    # Check the existence of tables 'Images' and 'Detected_Objects'
+    tables = ["Images", "Detected_Objects"]
+    for table in tables:
+        cursor.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name=?;", (table,)
+        )
+        table_exists = cursor.fetchone() is not None
+        assert table_exists
 
-def test_add_detected_objects(mock_sqlite3):
-    mock_conn, mock_cursor = mock_sqlite3
-    mock_cursor.fetchone.return_value = [0]  # Simulating object not existing
 
-    indexing = SQLiteIndexing()
-    indexing.add_detected_objects('test.jpg', ['object1', 'object2'])
+def test_add_image_path(sqlite_indexing_connection_cursor):
+    sqlite_index, _, cursor = sqlite_indexing_connection_cursor
 
-    expected_calls = [
-        call(INSERT_DETECTED_OBJECTS_PARAM_QUERY, ('test.jpg', 'object1')),
-        call(INSERT_DETECTED_OBJECTS_PARAM_QUERY, ('test.jpg', 'object2'))
-    ]
-    mock_cursor.execute.assert_has_calls(expected_calls)
-    assert mock_conn.commit.call_count == len(['object1', 'object2'])
+    image_path = "example_images/image1.jpg"
+    sqlite_index.add_image_path(image_path)
 
-# Additional tests can be written for get_images_with_all_objects, get_images_with_some_objects, and get_all_images_and_objects
+    select_image_path_query = (
+        "SELECT image_path FROM Images WHERE image_path = 'example_images/image1.jpg';"
+    )
+    cursor.execute(select_image_path_query)
+
+    result = cursor.fetchone()
+    assert result is not None
+
+
+def test_add_detected_objects(sqlite_indexing_connection_cursor):
+    sqlite_index, _, cursor = sqlite_indexing_connection_cursor
+
+    detected_objects = ["car"]
+    image_path = "example_images/image4.jpg"
+
+    sqlite_index.add_detected_objects(image_path, detected_objects)
+
+    # Complete the query to check for the detected objects at the image_path
+    query = """
+    SELECT * 
+    FROM Detected_Objects 
+    WHERE image_path = ? AND detected_object = ?;
+    """
+
+    cursor.execute(
+        query, (image_path, detected_objects[0])
+    )  # Assuming the first element is 'car'
+
+    result = cursor.fetchone()
+    assert result is not None
+
+
+def test_get_images_with_all_objects(
+    sqlite_indexing_connection_cursor, insert_initial_data
+):
+    sqlite_index, _, _ = sqlite_indexing_connection_cursor
+
+    objects_to_find = ("chair", "dining table")
+    result_image_and_path = sqlite_index.get_images_with_all_objects(objects_to_find)
+    expected_image_and_paths = {"example_images/image1.jpg": ["chair", "dining table"]}
+    assert result_image_and_path == expected_image_and_paths
+
+
+def test_get_images_with_some_objects(
+    sqlite_indexing_connection_cursor, insert_initial_data
+):
+    sqlite_index, _, _ = sqlite_indexing_connection_cursor
+
+    objects_to_find = ("potted plant", "dining table")
+    result_image_and_path = sqlite_index.get_images_with_some_objects(objects_to_find)
+    expected_image_and_paths = {"example_images/image1.jpg": ["chair", "dining table"]}
+    assert result_image_and_path == expected_image_and_paths
+
+
+def test_get_all_images_and_objects(
+    sqlite_indexing_connection_cursor, insert_initial_data
+):
+    sqlite_index, _, _ = sqlite_indexing_connection_cursor
+
+    results = sqlite_index.get_all_images_and_objects()
+    example_result = {"example_images/image1.jpg": ["chair", "dining table"]}
+    assert results == example_result
