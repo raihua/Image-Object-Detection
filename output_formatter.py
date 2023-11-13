d9e13ff418f2bc65d4efec336592612d831c40e5
raihua <raihua.dev@gmail.com>
2023-11-13 18:36:50 +1030

    refactor outputter

--- output_formatter.py
+++ output_formatter.py
@@ -11,39 +11,11 @@ class OutputFormatter:
 
     def format_data(self, image_data, k=None) -> str:
         formatted_data = self.__strategy.format_data(image_data)
-
-        if k is not None:
-            k_results_data = self.__get_top_k_results(formatted_data, k)
-        else:
-            k_results_data = formatted_data
-
-        formatted_str = None
-
-        if isinstance(self.__strategy, AlphabeticalAscendingFormat):
-            formatted_str = "\n".join(
-                "{}: {}".format(filename, ",".join(objects))
-                for filename, objects in k_results_data.items()
-            )
-        elif isinstance(self.__strategy, NumDescendingFormat):
-            formatted_str = "\n".join(
-                "{:.4f} {}".format(value, filename)
-                for filename, value in k_results_data.items()
-            )
-        else:
-            formatted_str = "\n".join(
-                "{}: {}".format(filename, ",".join(objects))
-                for filename, objects in k_results_data.items()
-            )
-
-        return formatted_str
+        k_results_data = self.__get_top_k_results(formatted_data, k) if k is not None else formatted_data
+        return self.__format_strategy_specific(k_results_data)
 
     def __get_top_k_results(self, image_data, k) -> dict:
-        if k is not None:
-            # Sort the formatted_data by values in descending order
-            sorted_data = dict(
-                sorted(image_data.items(), key=lambda item: item[1], reverse=True)
-            )
-            k_results = dict(list(sorted_data.items())[:k])
-            return k_results
-        else:
-            return image_data
+        return dict(sorted(image_data.items(), key=lambda item: item[1], reverse=True)[:k])
+
+    def __format_strategy_specific(self, data) -> str:
+        return self.__strategy.format_output(data)
